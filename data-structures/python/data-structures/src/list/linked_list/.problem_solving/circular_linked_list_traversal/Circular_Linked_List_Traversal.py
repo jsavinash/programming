aeba567d4bd618d2e17153d73e2947796f17cd34
“Avinash <avinashpremnarayan.nishad@sony.com>
2025-04-05 03:27:20 +0530

    code added

---
+++ data-structures/python/data-structures/src/list/linked_list/.problem_solving/circular_linked_list_traversal/Circular_Linked_List_Traversal.py
@@ -0,0 +1,24 @@
+from src.list.linked_list.circular_linked_list.Circular_Singly_Linked_List import CircularSinglyLinkedList
+
+
+class CircularLinkedListTraversal(CircularSinglyLinkedList):
+    def traversal(self):
+        temp_head = self.head
+        while temp_head is not None:
+            print(f"{temp_head.data} => ", end="")
+            if temp_head.next == self.head:
+                temp_head = None
+            else:
+                temp_head = temp_head.next
+
+        print()
+
+
+circularLinkedListTraversal = CircularLinkedListTraversal()
+print("Circular Linked List Traversal")
+circularLinkedListTraversal.add_data(1)
+circularLinkedListTraversal.add_data(5)
+circularLinkedListTraversal.add_data(5)
+circularLinkedListTraversal.add_data(5)
+circularLinkedListTraversal.add_data(6)
+circularLinkedListTraversal.traversal()
