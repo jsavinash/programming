3ce8c3ff75a92c1fbb2d371ca6c730d061631435
jsavinash <js.avinash.nishad@gmail.com>
2025-05-10 20:01:20 +0530

    Matrix

---
+++ data-structures/python/data-structures/src/array/implementation/matrix/README.md
@@ -0,0 +1,2 @@
+# Matrix
+![alt text](../../../../../../assets/svg/Matrix.svg "Title")
--- data-structures/python/data-structures/src/array/implementation/matrix/type_of_matrix/Diagonal_Matrix.py
+++ data-structures/python/data-structures/src/array/implementation/matrix/type_of_matrix/Diagonal_Matrix.py
@@ -1,11 +1,31 @@
-# Matrix with m(Row) x n(Column). 
-# Diagonal matrix : Accept principal diagonal element of matrix are not zero, rest all are zero.
+# Matrix with m(Row) x n(Column).
+# Diagonal matrix : Multiplication of matrix and it's transpose will give identity matrix.
 class DiagonalMatrix:
-    def diagonal_matrix(self):
-        return [[1, 0, 0, 0], [0, 3, 0, 0], [0, 0, 8, 0], [0, 0, 0, 9]]
+    def matrix(self):
+        return [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
-    def anti_diagonal_matrix(self):
-        return [[0, 0, 0, 1], [0, 0, 7, 0], [0, 9, 0, 0], [11, 0, 0, 0]]
+    def create_transpose_matrix(self, matrix):
+        new_matrix = []
+        row = len(matrix)
+        col = len(matrix[0])
+        for x_col in range(col):
+            x_new_row = []
+            for x_row in range(row):
+                x_new_row.append(matrix[x_row][x_col])
+            new_matrix.append(x_new_row)
+        return new_matrix
+
+    def multiply_matrix(self, matrix, matrix_2):
+        new_matrix = []
+        new_row = []
+        sum = 0
+        for w in range(matrix):
+            for x in range(matrix[0]):
+                for y in range(matrix_2):
+                    for z in range(matrix_2[0]):
+                        sum = sum + matrix[w][x] * matrix_2[z][y]
+        new_row.append(sum)
+        return new_matrix
 
     def print(self, matrix):
         print("[")
@@ -15,5 +35,9 @@ class DiagonalMatrix:
 
 
 diagonal_matrix = DiagonalMatrix()
-diagonal_matrix.print(diagonal_matrix.diagonal_matrix())
-diagonal_matrix.print(diagonal_matrix.anti_diagonal_matrix())
+matrix = diagonal_matrix.matrix()
+matrix_transpose = diagonal_matrix.create_transpose_matrix(diagonal_matrix.matrix())
+multiply_matrix = diagonal_matrix.multiply_matrix(matrix, matrix_transpose)
+diagonal_matrix.print(matrix)
+diagonal_matrix.print(matrix_transpose)
+diagonal_matrix.print(multiply_matrix)
