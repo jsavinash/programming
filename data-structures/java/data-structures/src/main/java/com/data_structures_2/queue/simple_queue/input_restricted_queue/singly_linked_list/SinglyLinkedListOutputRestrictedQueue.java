cd77db875f3ffdf4ad936c15e83122b3c8209b7a
jsavinash <js.avinash.nishad@gmail.com>
2025-05-10 20:25:26 +0530

    Queue structure added

---
+++ data-structures/java/data-structures/src/main/java/com/data_structures_2/queue/simple_queue/input_restricted_queue/singly_linked_list/SinglyLinkedListOutputRestrictedQueue.java
@@ -0,0 +1,4 @@
+package com.data_structures_2.queue.simple_queue.input_restricted_queue.singly_linked_list;
+
+public class SinglyLinkedListOutputRestrictedQueue {
+}
---
+++ data-structures/java/data-structures/src/main/java/com/data_structures_2/queue/simple_queue/output_restricted_queue/doubly_linked_list/DoublyLinkedListOutputRestrictedQueue.java
@@ -0,0 +1,4 @@
+package com.data_structures_2.queue.simple_queue.output_restricted_queue.doubly_linked_list;
+
+public class DoublyLinkedListOutputRestrictedQueue {
+}
