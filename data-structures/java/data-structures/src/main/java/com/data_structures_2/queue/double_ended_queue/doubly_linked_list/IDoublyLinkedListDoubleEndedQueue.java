3cd38e7e95c96b028709904c39a187cbd75fff92
jsavinash <js.avinash.nishad@gmail.com>
2025-05-19 14:41:26 +0530

    Doubly linked list double ended queue

---
+++ data-structures/java/data-structures/src/main/java/com/data_structures_2/queue/double_ended_queue/doubly_linked_list/IDoublyLinkedListDoubleEndedQueue.java
@@ -0,0 +1,4 @@
+package com.data_structures_2.queue.double_ended_queue.doubly_linked_list;
+
+public interface IDoublyLinkedListDoubleEndedQueue {
+}
---
+++ data-structures/java/data-structures/src/main/java/com/data_structures_2/queue/double_ended_queue/singly_linked_list/SinglyLinkedListDoubleEndedQueue.java
@@ -0,0 +1,4 @@
+package com.data_structures_2.queue.double_ended_queue.singly_linked_list;
+
+public class SinglyLinkedListDoubleEndedQueue {
+}
