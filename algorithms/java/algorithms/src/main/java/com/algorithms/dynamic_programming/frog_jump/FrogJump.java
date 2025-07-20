8bab38db1022386bfc04751f160711c06603fc82
jsavinash <js.avinash.nishad@gmail.com>
2025-07-21 02:48:23 +0530

    Frog jump

--- algorithms/java/algorithms/src/main/java/com/algorithms/dynamic_programming/frog_jump/FrogJump.java
+++ algorithms/java/algorithms/src/main/java/com/algorithms/dynamic_programming/frog_jump/FrogJump.java
@@ -13,9 +13,22 @@ public class FrogJump {
         }
     }
 
+    double maximumEnergyRequired(int[] staircase, int idx) {
+        if (idx == 0) {
+            return 0;
+        } else {
+            double oneStepBackFromEnd = maximumEnergyRequired(staircase, idx - 1) + Math.abs(staircase[idx] - staircase[idx - 1]);
+            double twoStepBackFromEnd = Double.NEGATIVE_INFINITY;
+            if (idx > 1)
+                twoStepBackFromEnd = maximumEnergyRequired(staircase, idx - 2) + Math.abs(staircase[idx] - staircase[idx - 2]);
+            return Math.max(oneStepBackFromEnd, twoStepBackFromEnd);
+        }
+    }
+
     public static void main(String args[]) {
         int[] staircase = {10, 20, 30, 10};
         FrogJump fj = new FrogJump();
         System.out.println(fj.minimumEnergyRequired(staircase, staircase.length - 1));
+        System.out.println(fj.maximumEnergyRequired(staircase, staircase.length - 1));
     }
 }
