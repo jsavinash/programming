6bce17e34e6212ba964c922a05f86566c832b4fa
jsavinash <js.avinash.nishad@gmail.com>
2025-07-21 02:46:24 +0530

    Frog jump

---
+++ algorithms/java/algorithms/src/main/java/com/algorithms/dynamic_programming/frog_jump/FrogJump.java
@@ -0,0 +1,21 @@
+package com.algorithms.dynamic_programming.frog_jump;
+
+public class FrogJump {
+    double minimumEnergyRequired(int[] staircase, int idx) {
+        if (idx == 0) {
+            return 0;
+        } else {
+            double oneStepBackFromEnd = minimumEnergyRequired(staircase, idx - 1) + Math.abs(staircase[idx] - staircase[idx - 1]);
+            double twoStepBackFromEnd = Double.POSITIVE_INFINITY;
+            if (idx > 1)
+                twoStepBackFromEnd = minimumEnergyRequired(staircase, idx - 2) + Math.abs(staircase[idx] - staircase[idx - 2]);
+            return Math.min(oneStepBackFromEnd, twoStepBackFromEnd);
+        }
+    }
+
+    public static void main(String args[]) {
+        int[] staircase = {10, 20, 30, 10};
+        FrogJump fj = new FrogJump();
+        System.out.println(fj.minimumEnergyRequired(staircase, staircase.length - 1));
+    }
+}
---
+++ algorithms/java/algorithms/src/main/java/com/algorithms/dynamic_programming/frog_jump/README.md
@@ -0,0 +1,24 @@
+### Fibonacci Sequence :-First and second number 0 and 1 can be used to generate fibonacci number by keep on adding previous 2 number.
+
+**Example : 0 1 1 2 3 5 8 13 21**
+
+**Problem Statement** : Get nth fibonacci number, where n = 5.
+
+> **Approaches**
+> * Brute force `Complexity => Time : O(2^n) | Space : Unknown`
+> * Memoization (Top-Down) `Complexity => Time : O(n) | Space : O(n)`
+> * Tabulation (Bottom-Up) `Complexity => Time : O(n) | Space : O(n)`
+> * Space optimization `Complexity => Time : O(n) | Space : O(1)`
+
+**Brute force Approach**
+
+![Brute force](../../../../../../../../../assets/svg/FrogJumpDp.svg)
+
+**Memoization (Top-Down) Approach**
+
+
+**Tabulation (Bottom-Up) Approach**
+
+
+**Space Optimization (Three Pointer Approach)**
+
