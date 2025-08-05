a8820a1bed699f3b5784f415b1caa1ada834f158
jsavinash <js.avinash.nishad@gmail.com>
2025-08-06 03:10:05 +0530

    Programming question

--- algorithms/java/algorithms/src/main/java/com/algorithms/dynamic_programming/grid_dp/MinimumPathSum.java
+++ algorithms/java/algorithms/src/main/java/com/algorithms/dynamic_programming/grid_dp/MinimumPathSum.java
@@ -13,9 +13,21 @@ public class MinimumPathSum {
         }
     }
 
+    int computeMinimumPathSumInGridTriangle(int row, int col, int[][] grid) {
+        if (row == grid.length - 1) {
+            return grid[grid.length - 1][col];
+        }  else {
+            int down = computeMinimumPathSumInGridTriangle(row + 1, col, grid);
+            int diagonal = computeMinimumPathSumInGridTriangle(row + 1, col + 1, grid);
+            return Math.min(down, diagonal) + grid[row][col];
+        }
+    }
+
     public static void main(String args[]) {
         MinimumPathSum mps = new MinimumPathSum();
         int[][] grid = {{5, 9, 6}, {11, 5, 2}};
+        int[][] gridTriangle = {{2}, {3, 7}, {8, 5, 6}, {6, 1, 9, 3}};
         System.out.println(mps.computeMinimumPathSumInGrid(grid.length - 1, grid[0].length - 1, grid));
+        System.out.println(mps.computeMinimumPathSumInGridTriangle(0, 0, gridTriangle));
     }
 }
